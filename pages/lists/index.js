fe3cbcb3b6d99492f6619707590efb19f5376932
givi-works <89351540+givi-works@users.noreply.github.com>
2022-07-28 17:57:20 +0400

    Update index.js

--- pages/lists/index.js
+++ pages/lists/index.js
@@ -1,40 +1,47 @@
-import { useRouter } from "next/router";
-import { useState } from "react";
-import ItemCard from "../../components/ItemCard";
-import Layout from "../../components/Layout";
-import axios from 'axios'
+// import { useRouter } from "next/router";
+// import { useState } from "react";
+// import ItemCard from "../../components/ItemCard";
+// import Layout from "../../components/Layout";
+// import axios from 'axios'
 
-export default function ListsPage({data}) {
-    const firstPage = data.slice(0,5)
-    const [listItems, setListItems] = useState(firstPage)
-    const router = useRouter()
-    const handleLoadMore = () => { 
-        const newLength = listItems.length + 5
-        const loaded = data.slice(0,newLength)
-        setListItems(loaded)
-    }
-    return (
-        <Layout> 
-            <div className="max-w-xl border-2 border-emerald-300 rounded-md m-auto px-6 py-4 shadow-cyan-500/50 shadow-md"> 
-            {
-                listItems.map(list => { 
-                    return(
-                        <div key={list._id} onClick={() => router.push(`/lists/${list.slug}`)} className='cursor-pointer'>
-                            <ItemCard data={list} />
-                        </div>
-                    )
-                })
-            }
-            <button onClick={() => handleLoadMore()} className='border-2 px-2 py-1'>Load more</button>
-            </div>
-        </Layout>
-    )
+// export default function ListsPage({data}) {
+//     const firstPage = data.slice(0,5)
+//     const [listItems, setListItems] = useState(firstPage)
+//     const router = useRouter()
+//     const handleLoadMore = () => { 
+//         const newLength = listItems.length + 5
+//         const loaded = data.slice(0,newLength)
+//         setListItems(loaded)
+//     }
+//     return (
+//         <Layout> 
+//             <div className="max-w-xl border-2 border-emerald-300 rounded-md m-auto px-6 py-4 shadow-cyan-500/50 shadow-md"> 
+//             {
+//                 listItems.map(list => { 
+//                     return(
+//                         <div key={list._id} onClick={() => router.push(`/lists/${list.slug}`)} className='cursor-pointer'>
+//                             <ItemCard data={list} />
+//                         </div>
+//                     )
+//                 })
+//             }
+//             <button onClick={() => handleLoadMore()} className='border-2 px-2 py-1'>Load more</button>
+//             </div>
+//         </Layout>
+//     )
+// }
+
+// export async function getStaticProps(){ 
+//     const res = await axios.get(`/api/lists`)
+//     const data = await res.json()
+//     return{
+//         props: {data}
+//     }
+// }
+
+export default function index() {
+  return (
+    <div>index</div>
+  )
 }
 
-export async function getStaticProps(){ 
-    const res = await axios.get(`/api/lists`)
-    const data = await res.json()
-    return{
-        props: {data}
-    }
-}
