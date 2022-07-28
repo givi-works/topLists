5492494773e4fecbc6f247063f4d45d23d35b79d
givi-works <89351540+givi-works@users.noreply.github.com>
2022-07-28 17:54:39 +0400

    Update index.js

--- pages/lists/index.js
+++ pages/lists/index.js
@@ -32,7 +32,7 @@ export default function ListsPage({data}) {
 }
 
 export async function getStaticProps(){ 
-    const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/lists`)
+    const res = await axios.get(`/api/lists`)
     const data = await res.json()
     return{
         props: {data}
