4d458b81680e9a7bdd2c31158bfd97601389ecb3
givi-works <89351540+givi-works@users.noreply.github.com>
2022-07-29 17:16:43 +0400

    Update signup.js

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,21 +1,22 @@
-import nc from "next-connect"
 import hashPassword from "../../../lib/auth"
 import openConnection, { closeConnection } from "../../../lib/db"
 import User from '../../../schemas/User'
 
-const handler = nc().post(async(req, res) => { 
-    await openConnection()
-    const { email, username, password } = req.body
-    try {
-        const hashedPassword = await hashPassword(password)
-        const newUser = new User({ email, username, password: hashedPassword })
-        await newUser.save()
-        res.status(201).json({ message: "created sucesfully" })
-        await closeConnection()
-    } catch (error) {
-        await closeConnection()
-        return res.status(400).json({ message: 'Something Went Wrong' })
+const handler = async (req, res) => { 
+    if(req.method == "POST") {
+        await openConnection()
+        const { email, username, password } = req.body
+        try {
+            const hashedPassword = await hashPassword(password)
+            const newUser = new User({ email, username, password: hashedPassword })
+            await newUser.save()
+            res.status(201).json({ message: "created sucesfully" })
+            await closeConnection()
+        } catch (error) {
+            await closeConnection()
+            return res.status(400).json({ message: 'Something Went Wrong' })
+        }
     }
-})
+}
 
 export default handler
